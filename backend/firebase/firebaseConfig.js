0ca934a249290722b4e6e9610932b2cc7bcd2792
ZamperliniCorporation <gustavohzq@gmail.com>
2025-07-07 11:02:08 -0300

    firebaseConfig.js

--- backend/firebase/firebaseConfig.js
+++ backend/firebase/firebaseConfig.js
@@ -1,10 +1,9 @@
-const admin = require("firebase-admin");
-
-const firebaseConfig = JSON.parse(process.env.FIREBASE_CONFIG);
+const admin = require('firebase-admin');
+const serviceAccount = require('./firebase.js'); // Certifique-se de que esse é o nome correto do seu arquivo JSON ou JS
 
 if (!admin.apps.length) {
   admin.initializeApp({
-    credential: admin.credential.cert(firebaseConfig)
+    credential: admin.credential.cert(serviceAccount)
   });
 }
 
@@ -12,7 +11,11 @@ const db = admin.firestore();
 
 async function salvarPlanoNoFirestore(nome, plano) {
   const docRef = db.collection('planos').doc(nome);
-  await docRef.set({ nome, plano, criadoEm: new Date() });
+  await docRef.set({
+    nome,
+    plano,
+    criadoEm: new Date()
+  });
 }
 
 module.exports = { salvarPlanoNoFirestore };
